c2c6c51b30794538672c0edf509e0117a7dc2f2c
mortuz <mortuzalam@gmail.com>
2018-09-09 22:11:02 +0530

    responsive home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
 (function($) {
     $('#hamburger').on('click', function() {
         $("body, html").toggleClass("open-nav");
-    })
+    });
+
+    $('.backdrop').on('click', function(){
+        $("body, html").removeClass("open-nav");
+    });
+    $('.nav-links').on('click', 'li', function(){
+        $("body, html").removeClass("open-nav");
+    });
 })(jQuery)
