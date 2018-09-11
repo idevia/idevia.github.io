35bc75ce53e2379118deeb7443619d20e6d5d7d4
mortuz <mortuzalam@gmail.com>
2018-09-11 12:17:09 +0530

    add meta tags

--- inc/meta-tags.php
+++ inc/meta-tags.php
@@ -1,7 +1,18 @@
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
+<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
+
+
+<meta property="og:type" content="business.business">
+<meta property="og:title" content="iDevia - Place to be more creative">
+<meta property="og:description" content="We deliver smart next - generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world.">
+<meta property="og:url" content="https://idevia.in">
+<meta property="og:image" content="https://idevia.in/assets/img/logoartb.png">
+<meta property="business:contact_data:country_name" content="India">
+
+<meta name="title" content="iDevia - Place to be more creative">
+<meta name="description" content="We deliver smart next - generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world.">
