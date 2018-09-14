cd40f653f4b17cadf70addfe6579e6e15da95da4
mortuz <mortuz@crmvideo.com>
2018-09-14 14:33:07 +0530

    update meta tags

--- inc/meta-tags.php
+++ inc/meta-tags.php
@@ -6,13 +6,25 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 
+<meta name="title" content="iDevia - Place to be more creative">
+<meta name="description" content="We are iDevia, where ideas come to life. Creative ideas that deliver tangible returns. We deliver smart next - generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world.">
+<meta name="keywords" content="web design, web application, ux/ui design, wordpress, ecommerce, graphic design, seo services, app development, digital marketing, content writing, angular, native script, electron, node js, php, slim framework, android, ios, desktop">
+<meta name="author" content="iDevia">
+<meta property="og:url" content="http://idevia.in/" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="iDevia - Place to be more creative" />
+<meta property="og:description" content="We deliver smart next-generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world." />
+<meta property="og:image" content="https://idevia.in/assets/img/logoartb.png">
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@idevia_in" />
+<meta name="twitter:creator" content="@idevia_in" />
 
-<meta property="og:type" content="business.business">
+<!-- <meta property="og:type" content="business.business">
 <meta property="og:title" content="iDevia - Place to be more creative">
 <meta property="og:description" content="We deliver smart next - generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world.">
 <meta property="og:url" content="https://idevia.in">
 <meta property="og:image" content="https://idevia.in/assets/img/logoartb.png">
-<meta property="business:contact_data:country_name" content="India">
+<meta property="business:contact_data:country_name" content="India"> 
 
 <meta name="title" content="iDevia - Place to be more creative">
-<meta name="description" content="We deliver smart next-generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world.">
+<meta name="description" content="We deliver smart next-generation solutions using advance technologies that combine excellence in design, quality and service. We use web design and online strategy to help brands get things done in the digital world.">-->
