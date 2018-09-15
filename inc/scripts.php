6286c92749c4f1ee2dd49a85859cf097bc7d3452
dhar1993 <38814523+dhar1993@users.noreply.github.com>
2018-09-15 13:00:53 +0530

    analyst

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,3 +1,14 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="./libs/owl-carousel/js/owl.carousel.min.js"></script>
 <script src="./assets/js/app.js"></script>
+
+<script>
+      (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-71919964-2', 'auto');
+    ga('send', 'pageview');
+  </script>
