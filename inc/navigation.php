ddeb531d1b6e9903d9c3855a7e0cbece8b32f6fe
mortuz <mortuzalam@gmail.com>
2019-06-23 21:50:02 +0530

    add portfolio

--- inc/navigation.php
+++ inc/navigation.php
@@ -10,6 +10,7 @@
             <li><a href="/">Home</a></li>
             <li><a href="about">About</a></li>
             <li><a href="services">Services</a></li>
+            <li><a href="./assets/docs/idevia-works.pdf" target="_blank">Portfolio</a></li>
             <li><a href="career">Career</a></li>
             <li><a href="contact">Contact</a></li>
         </ul>
